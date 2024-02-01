3d9f8ac33837b5010db27a431be2a10b7f887fe4
Morten <morten@scripts.ee>
2024-02-01 06:24:48 +0200

    test

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// This is a comment to my code
 
+// Rakendus küsib kasutajalt tema nime
+// ja tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name: ");
+// string - sõne
+string userName = Console.ReadLine();
+
+// string interpolation
+Console.WriteLine($"Hello, {userName}!");
